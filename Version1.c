1917297d4d25a1a1f63f7c4eb2f0072a37eaa558
sanket <narvekarsanket63@gmail.com>
2022-12-31 19:30:11 +0530

    additions

--- Version1.c
+++ Version1.c
@@ -6,7 +6,7 @@
  typedef struct emp{
 	struct emp *next;
 	struct emp *prev;
-	char empid[10]; // employee id
+	int empid; // employee id
 	char did[4]; // dep id
 	char first_name[15]; ///first name of employee
 	char last_name[20]; // last name of employee
@@ -29,7 +29,6 @@
 int main(){
     /*FILE *fp, *ft, *fd, *fdt; /// file pointers*/
     int choice;
-    int len, recfound=0;
 
     emp *hr1; /// structure variable creation
     emp *it1; 
@@ -51,11 +50,8 @@ int main(){
     Finance->depid = 1003;
     Sales->depid = 1004;
 
-
-
-
-
     while(1){
+    main_menu:
     system("cls"); ///clear the console window
 	printf("**************** Employee Management System *************\n");
     printf("*                                                       *\n");
@@ -73,30 +69,31 @@ int main(){
 	printf("*                                                       *\n");
 	printf("*******************************************************\n\n");
 	printf("Your Choice: ");
-
     fflush(stdin); /// flush the input buffer
 	scanf("%d",&choice); /// get the input from keyboard
-		switch(choice){
-			case 1:  /// if user press 1
-			system("cls");
-            printf("****************        HR DEPARTMENT       *************\n");
-            printf("*                                                       *\n");
-            printf("*               PLEASE SELECT AN OPTIONS                *\n");
-            printf("*                                                       *\n");
-            printf("* 1. ADD                                                *\n");
-            printf("*                                                       *\n");
-            printf("* 2. DELETR                                             *\n");
-            printf("*                                                       *\n");
-            printf("* 3. VIEW                                               *\n");
-            printf("*                                                       *\n");
-            printf("* 4. MODIFY                                             *\n");
-            printf("*                                                       *\n");
-            printf("* 5. MAIN MENU                                          *\n");
-            printf("*                                                       *\n");
-            printf("*******************************************************\n\n");
+	switch(choice){
+	case 1:  /// if user press 1
+        while(1){
+        HR_dep:
+		system("cls");
+        printf("****************        HR DEPARTMENT       *************\n");
+        printf("*                                                       *\n");
+        printf("*               PLEASE SELECT AN OPTION                *\n");
+        printf("*                                                       *\n");
+        printf("* 1. ADD                                                *\n");
+        printf("*                                                       *\n");
+        printf("* 2. DELETR                                             *\n");
+        printf("*                                                       *\n");
+        printf("* 3. VIEW                                               *\n");
+        printf("*                                                       *\n");
+        printf("* 4. MODIFY                                             *\n");
+        printf("*                                                       *\n");
+        printf("* 5. MAIN MENU                                          *\n");
+        printf("*                                                       *\n");
+        printf("*******************************************************\n\n");
 
-             fflush(stdin); /// flush the input buffer
-		choice  = getche(); /// get the input from keyboard
+        fflush(stdin); /// flush the input buffer
+		scanf("%d",&choice); /// get the input from keyboard
 		switch(choice){
 			case 1:  /// if user press 1
 			system("cls");
@@ -148,35 +145,342 @@ int main(){
 
                     strcpy(temp->first_name, fname);
                     strcpy(temp->last_name, lname);
+                break;
 
-
-                    
-
-                }
+                case 2:
+                    printf("enter the new employee ID\n");
+                    int id1;
+                    fflush(stdin);
+                    scanf("%d",&id1);
+                    temp->empid = id1;
+                break;
+                case 3:
+                    printf("enter the new age\n");
+                    fflush(stdin);
+                    scanf("%d",&temp->age);
+                break;
+                case 4:
+                    printf("enter the new basic salary\n");
+                    fflush(stdin);
+                    scanf("%d",&temp->basic_salary);
+                break;
+                case 5:
+                    goto HR_dep;
+                break;            
+              }
             }
-
-
-
+            }
         }
+    break;
+        
+	case 2:
+        while(1){
+        ITdep:
+        system("cls");
+        printf("****************        IT DEPARTMENT       *************\n");
+        printf("*                                                       *\n");
+        printf("*               PLEASE SELECT AN OPTION                *\n");
+        printf("*                                                       *\n");
+        printf("* 1. ADD                                                *\n");
+        printf("*                                                       *\n");
+        printf("* 2. DELETR                                             *\n");
+        printf("*                                                       *\n");
+        printf("* 3. VIEW                                               *\n");
+        printf("*                                                       *\n");
+        printf("* 4. MODIFY                                             *\n");
+        printf("*                                                       *\n");
+        printf("* 5. MAIN MENU                                          *\n");
+        printf("*                                                       *\n");
+        printf("*******************************************************\n\n");
+
+        fflush(stdin); /// flush the input buffer
+		scanf("%d",&choice); /// get the input from keyboard
+		switch(choice){
+			case 1:  /// if user press 1
+			system("cls");
+            it1 = add(it1);
+            IT->e = it1;                                   
+			break;
 
 			case 2:
 			system("cls");
+            delete(it1);
 			
 			break;
 
 			case 3:  /// if user press 3 then do editing existing record
 			system("cls");
-    
+            printList(it1);    
+			break;
+
+            case 4:
+            printf("Enter the employee ID you wish to modify\n");
+            int id; 
+            emp *temp = it1; 
+            scanf("%d",&id);
+            while(temp->empid!= id){
+                temp = temp->next;
+            }
+
+            while(1){
+            printf("Select option to modify\n");
+            printf("1. Name \n");
+            printf("2. Employee ID\n");
+            printf("3. Age\n");
+            printf("4. Basic Salary\n");
+            printf("5. Go back\n");
+            printf("Enter your choice\n");
+            fflush(stdin);
+            int k; 
+            scanf("%d",&k);
+
+            switch(k){
+            case 1:
+                printf("Enter the new First name\n");
+                char fname[50], lname[50]; 
+                fflush(stdin);
+                fgets(fname, 50, stdin);
+                printf("Enter the new Last name\n");
+                fflush(stdin);
+                fgets(lname, 50, stdin);
+                strcpy(temp->first_name, fname);
+                strcpy(temp->last_name, lname);
+            break;
+
+            case 2:
+                printf("enter the new employee ID\n");
+                int id1;
+                fflush(stdin);
+                scanf("%d",&id1);
+                temp->empid = id1;
+            break;
+            case 3:
+                printf("enter the new age\n");
+                fflush(stdin);
+                scanf("%d",&temp->age);
+            break;
+            case 4:
+                printf("enter the new basic salary\n");
+                fflush(stdin);
+                scanf("%d",&temp->basic_salary);
+            break;
+            case 5:
+                goto ITdep;
+            break;
+                  
+              }
+            }
+        }
+        break;
+
+            case 5:
+                goto main_menu;
+            break;
+        }			
+	break;
+
+	case 3:  /// if user press 3 then do editing existing record
+        while(1){
+        findep:
+		system("cls");
+        printf("****************    FINANCE DEPARTMENT      *************\n");
+        printf("*                                                       *\n");
+        printf("*               PLEASE SELECT AN OPTIONS                *\n");
+        printf("*                                                       *\n");
+        printf("* 1. ADD                                                *\n");
+        printf("*                                                       *\n");
+        printf("* 2. DELETR                                             *\n");
+        printf("*                                                       *\n");
+        printf("* 3. VIEW                                               *\n");
+        printf("*                                                       *\n");
+        printf("* 4. MODIFY                                             *\n");
+        printf("*                                                       *\n");
+        printf("* 5. MAIN MENU                                          *\n");
+        printf("*                                                       *\n");
+        printf("*******************************************************\n\n");
+
+        fflush(stdin); /// flush the input buffer
+		scanf("%d",&choice); /// get the input from keyboard
+		switch(choice){
+			case 1:  /// if user press 1
+			system("cls");
+            fin1 = add(fin1);
+            Finance->e = fin1;                                   
+			break;
+
+			case 2:
+			system("cls");
+            delete(fin1);			
+			break;
+
+			case 3:  /// if user press 3 then do editing existing record
+			system("cls");
+            printList(fin1);    
+			break;
+
+            case 4:
+            printf("Enter the employee ID you wish to modify\n");
+            int id; 
+            emp *temp = fin1; 
+            scanf("%d",&id);
+            while(temp->empid!= id){
+                temp = temp->next;
+            }
+
+            while(1){
+            printf("Select option to modify\n");
+            printf("1. Name \n");
+            printf("2. Employee ID\n");
+            printf("3. Age\n");
+            printf("4. Basic Salary\n");
+            printf("5. Go back\n");
+            printf("Enter your choice\n");
+            fflush(stdin);
+            int k; 
+            scanf("%d",&k);
+
+                switch(k){
+                case 1:
+                    printf("Enter the new First name\n");
+                    char fname[50], lname[50]; 
+                    fflush(stdin);
+                    fgets(fname, 50, stdin);
+                    printf("Enter the new Last name\n");
+                    fflush(stdin);
+                    fgets(lname, 50, stdin);
+
+                    strcpy(temp->first_name, fname);
+                    strcpy(temp->last_name, lname);
+                break;
+
+                case 2:
+                    printf("enter the new employee ID\n");
+                    fflush(stdin);
+                    scanf("%d",temp->empid);
+                break;
+                case 3:
+                    printf("enter the new age\n");
+                    fflush(stdin);
+                    scanf("%d",&temp->age);
+                break;
+                case 4:
+                    printf("enter the new basic salary\n");
+                    fflush(stdin);
+                    scanf("%d",&temp->basic_salary);
+                break;
+                case 5:
+                    goto findep;
+                break;
+                  
+              }
+            }
+        }    
     }
+    break;
+    case 4:
+        while(1){
+        Salesdep:
+		system("cls");
+        printf("****************     SALES DEPARTMENT       *************\n");
+        printf("*                                                       *\n");
+        printf("*               PLEASE SELECT AN OPTIONS                *\n");
+        printf("*                                                       *\n");
+        printf("* 1. ADD                                                *\n");
+        printf("*                                                       *\n");
+        printf("* 2. DELETR                                             *\n");
+        printf("*                                                       *\n");
+        printf("* 3. VIEW                                               *\n");
+        printf("*                                                       *\n");
+        printf("* 4. MODIFY                                             *\n");
+        printf("*                                                       *\n");
+        printf("* 5. MAIN MENU                                          *\n");
+        printf("*                                                       *\n");
+        printf("*******************************************************\n\n");
+        fflush(stdin); /// flush the input buffer
+		scanf("%d",&choice); /// get the input from keyboard
+		switch(choice){
+			case 1:  /// if user press 1
+			system("cls");
+            fin1 = add(sales1);
+            Sales->e = sales1;                                   
+			break;
 
+			case 2:
+			system("cls");
+            delete(sales1);
+			
+			break;
 
-    char empId[10]; /// string to store name of the employee
+			case 3:  /// if user press 3 then do editing existing record
+			system("cls");
+            printList(sales1);    
+			break;
 
-    char depId[10]; /// string to store name of the department
+            case 4:
+            printf("Enter the employee ID you wish to modify\n");
+            int id; 
+            emp *temp = sales1; 
+            scanf("%d",&id);
+            while(temp->empid!= id){
+                temp = temp->next;
+            }
+
+            while(1){
+            printf("Select option to modify\n");
+            printf("1. Name \n");
+            printf("2. Employee ID\n");
+            printf("3. Age\n");
+            printf("4. Basic Salary\n");
+            printf("5. Go back\n");
+            printf("Enter your choice\n");
+            fflush(stdin);
+            int k; 
+            scanf("%d",&k);
+
+                switch(k){
+                case 1:
+                    printf("Enter the new First name\n");
+                    char fname[50], lname[50]; 
+                    fflush(stdin);
+                    fgets(fname, 50, stdin);
+                    printf("Enter the new Last name\n");
+                    fflush(stdin);
+                    fgets(lname, 50, stdin);
+
+                    strcpy(temp->first_name, fname);
+                    strcpy(temp->last_name, lname);
+                break;
+
+                case 2:
+                    printf("enter the new employee ID\n");
+                    fflush(stdin);
+                    scanf("%d",temp->empid);
+                break;
+                case 3:
+                    printf("enter the new age\n");
+                    fflush(stdin);
+                    scanf("%d",&temp->age);
+                break;
+                case 4:
+                    printf("enter the new basic salary\n");
+                    fflush(stdin);
+                    scanf("%d",&temp->basic_salary);
+                break;
+                case 5:
+                    goto Salesdep;
+                break;
+                  
+              }
+            }
+        }    
+    }
+    break; 
+
+    }
 }
 }
 
-struct emp *add(struct emp *head){
+emp* add(emp* head){
     system("cls");
     emp *ptr = (emp*)(malloc(sizeof(emp)));
 
@@ -189,7 +493,7 @@ struct emp *add(struct emp *head){
         printf("Enter the employee's Last name\n");
         scanf("%s",&ptr->last_name);
         printf("Enter the employee ID\n");
-        scanf("%s",&ptr->empid);
+        scanf("%d",&ptr->empid);
         printf("Enter the employee's Department ID\n");
 	    scanf("%s",&ptr->did);
         printf("Enter the employee's Age\n");
@@ -219,10 +523,9 @@ struct emp *add(struct emp *head){
         tail = ptr; 
     }
     return head;
+}
 
-    }
-
-    emp* delete(emp* head){
+emp *delete (emp* head){
     printf("\nEnter the full name of the employee you want to remove\n");
     char name[50];
     gets(name);
@@ -236,7 +539,7 @@ struct emp *add(struct emp *head){
 
     while(ptr->next!= NULL)
     {
-        int a = strcmp(strcat(ptr->last_name, ptr->first_name), name);
+        int a = strcmp(strcat(strcat(ptr->last_name," "), ptr->first_name), name);
    
         if (a==0) 
         {
